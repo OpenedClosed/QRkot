ce975fe2cbaf8c16b196e02b59e60f85d97cf057
OpenedClosed <opendoor200179@gmail.com>
2023-05-10 11:04:27 +0300

    first after review

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -7,37 +7,38 @@ from app.core.config import settings
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 
+SPREADSHEET_BODY = {
+    'properties': {
+        'title': 'Отчет на данный момент',
+        'locale': 'ru_RU'
+    },
+    'sheets': [
+        {
+            'properties': {
+                'sheetType': 'GRID',
+                'sheetId': 0,
+                'title': 'Лист1',
+                'gridProperties': {
+                    'rowCount': 100,
+                    'columnCount': 11
+                }
+            }
+        }
+    ]
+}
+
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     """Создание таблицы."""
     now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = {
-        'properties': {
-            'title': f'Отчет на {now_date_time}',
-            'locale': 'ru_RU'
-        },
-        'sheets': [
-            {
-                'properties': {
-                    'sheetType': 'GRID',
-                    'sheetId': 0,
-                    'title': 'Лист1',
-                    'gridProperties': {
-                        'rowCount': 100,
-                        'columnCount': 11
-                    }
-                }
-            }
-        ]
-    }
+    spreadsheet_body = SPREADSHEET_BODY
+    spreadsheet_body['properties']['title'] = f'Отчет на {now_date_time}'
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    spreadsheetid = response['spreadsheetId']
-    print(f'Ссылка на сводную таблицу: '
-          f'https://docs.google.com/spreadsheets/d/{spreadsheetid}')
-    return spreadsheetid
+    spreadsheet_id = response['spreadsheetId']
+    return spreadsheet_id
 
 
 async def set_user_permissions(
